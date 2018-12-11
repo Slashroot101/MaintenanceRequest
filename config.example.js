eab4119f6d70a469f7e47de0a9c35bb766814c85
Samuel Curry <samc735@gmail.com>
2018-12-11 14:26:33 -0500

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+    forge: {
+        client_id : //FORGE CLIENT ID
+        client_secret: //FORGE SECRET
+        scopePublic: //PERMISSIONS FOR FORGE
+        },
+    mongo: {
+        host: //MONGO HOST
+        username: //MONGO USER
+        authdb: //MONGO AUTH DB
+        password: //MONGO PASS
+    },
+    ip: //IP FOR WEBSOCKET TO LISTEN ON
+};
